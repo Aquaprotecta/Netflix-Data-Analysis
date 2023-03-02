949d05ed9721e5c301ee856667dbf29ea906acb0
Aquaprotecta <239863@edu.rocfriesepoort.nl>
2023-03-02 14:30:17 +0100

    Update 1

--- Netflix Data Analysis/MainWindow.xaml.cs
+++ Netflix Data Analysis/MainWindow.xaml.cs
@@ -18,7 +18,7 @@ namespace Netflix_Data_Analysis
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         public MainWindow()
         {
