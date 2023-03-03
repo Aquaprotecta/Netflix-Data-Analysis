5887cc728d38692089b607dafde20dade4035b92
Aquaprotecta <239863@edu.rocfriesepoort.nl>
2023-03-03 14:18:40 +0100

    Started on views

---
+++ Netflix Data Analysis/Core/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace Netflix_Data_Analysis.Core
+{
+    class ObservableObject : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Netflix Data Analysis/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,34 @@
+﻿using Netflix_Data_Analysis.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Netflix_Data_Analysis.MVVM.ViewModel
+{
+    class MainViewModel : ObservableObject
+    {
+
+        public HomeViewModel HomeVM { get; set; }
+
+        private object _currentView;
+
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set
+            {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            HomeVM = new HomeViewModel();
+            CurrentView = HomeVM;
+
+        }
+    }
+}
